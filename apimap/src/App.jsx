e7fc98e0e5134b381998626bc1b0a137eb8a5e54
MariaClaravalotti <valottimaria@gmail.com>
2025-01-25 12:11:11 -0300

    estilização da pag

--- apimap/src/App.jsx
+++ apimap/src/App.jsx
@@ -1,21 +1,26 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import axios from 'axios';
 
 export default function App() {
   const [desenhos, setDesenhos] = useState([]); // Armazena todos os personagens
 
-  const pagarDados = async () => {
+  const pegarDados = async () => {
     const Dados = await axios.get('https://api.sampleapis.com/rickandmorty/characters');
     setDesenhos(Dados.data); // Armazena o array completo
   };
 
   useEffect(() => {
-    pagarDados();
+    pegarDados();
   }, []);
 
   return (
     <>
+    <header>
+      <div>
+      <img className="image" src="https://licensingcon.com.br/wp-content/uploads/2019/08/rick.png" alt="" />
+      </div>
       <h1>API + Map</h1>
+    </header>
 
       <section style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
         {/* Renderizando todos os personagens */}
@@ -26,6 +31,22 @@ export default function App() {
           </article>
         ))}
       </section>
+      <footer style={{ position: "relative", overflow: "hidden", height: "50px", backgroundColor: "#333" }}>
+        <div
+          style={{
+            display: "inline-block",
+            whiteSpace: "nowrap",
+            animation: "marquee 15s linear infinite",
+            color: "#fff",
+            fontSize: "1rem",
+            paddingLeft: "100%",
+          }}
+        >
+          <span style={{ marginRight: "50px" }}><a href="https://www.linkedin.com/in/maria-clara-valotti-2869b51ab/">Linkedin</a>| </span>
+          <span style={{ marginRight: "50px" }}>API + Map com React | </span>
+          <span style={{ marginRight: "50px" }}>Maria Clara Valotti</span>
+        </div>
+      </footer>
     </>
   );
 }
